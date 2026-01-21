f21ad0238106ffa8f723cfdba747be2c4e4db0ad
Aleph0Milk <sepioariei@gmail.com>
2026-01-22 01:04:48 +0900

    CompressMod.java を更新

--- src/main/java/com/aleph0milk/compress/CompressMod.java
+++ src/main/java/com/aleph0milk/compress/CompressMod.java
@@ -1,26 +1,35 @@
 package com.aleph0milk.compress;
 
+import net.minecraft.world.item.Item;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-// gradle.propertiesで設定したmod_idと一致させる必要があります
-@Mod("alephs_compress")
+@Mod(CompressMod.MODID)
 public class CompressMod {
     public static final String MODID = "alephs_compress";
     public static final Logger LOGGER = LogManager.getLogger();
 
+    // アイテム登録のためのレジストリ
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+
+    // 圧縮アイテム本体の登録
+    public static final RegistryObject<Item> COMPRESSED_ITEM = ITEMS.register("compressed_item", 
+        () -> new CompressedItem(new Item.Properties()));
+
     public CompressMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        // 今後、アイテムやブロックを登録する際にここを使用します
+        // レジストリをイベントバスに登録
+        ITEMS.register(modEventBus);
         
-        // システムイベント（設置不可の処理など）を登録するための準備
         MinecraftForge.EVENT_BUS.register(this);
-        
         LOGGER.info("Compress! Mod has been initialized.");
     }
 }
